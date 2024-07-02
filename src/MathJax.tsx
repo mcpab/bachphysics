1137750a78a754eb3f941705e462a4a97488618a
Marco Brunelli <mcpab68@gmail.com>
2024-07-02 16:30:44 -0400

    updated load of scripts

--- src/MathJax.tsx
+++ src/MathJax.tsx
@@ -7,12 +7,12 @@ export default function MathJax() {
 
     return (
         <>
-            <Script src='/js/MathJaxConfig.js' strategy="afterInteractive" onReady={() => {
+            <Script src='/js/MathJaxConfig.js' strategy="lazyOnload" onReady={() => {
                 console.log('loaded config');
             }}
                 defer />
 
-            <Script src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js" strategy="afterInteractive" onReady={() => {
+            <Script src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js" strategy="lazyOnload" onReady={() => {
                 console.log('loaded MathJax');
             }}
                 defer />
