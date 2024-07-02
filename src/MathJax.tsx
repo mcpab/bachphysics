ae1fe97cb5405d81cd8802fd387e060bb346fd56
Marco Brunelli <mcpab68@gmail.com>
2024-07-02 18:50:17 -0400

    Update MathJax.tsx

--- src/MathJax.tsx
+++ src/MathJax.tsx
@@ -7,12 +7,24 @@ export default function MathJax() {
 
     return (
         <>
-            <Script src='/js/MathJaxConfig.js' strategy="lazyOnload" onReady={() => {
+            <Script>
+                {`MathJax = {
+  loader: {load: ['[tex]/physics']},
+    tex: {
+      tags: 'ams',
+      packages: {'[+]': ['physics']}
+    }
+  };
+  console.log('loaded config');`
+                }
+            </Script>
+
+           {/*  <Script src='/js/MathJaxConfig.js' strategy="lazyOnload" onReady={() => {
                 console.log('loaded config');
             }}
-                defer />
+                defer /> */}
 
-            <Script src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js" strategy="lazyOnload" onReady={() => {
+            <Script src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js" async strategy="lazyOnload" onReady={() => {
                 console.log('loaded MathJax');
             }}
                 defer />
