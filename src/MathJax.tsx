337ccd22d84bcedfa7661304ea4fed9422a23eb4
Marco Brunelli <mcpab68@gmail.com>
2024-07-02 21:01:17 -0400

    Update MathJax.tsx

--- src/MathJax.tsx
+++ src/MathJax.tsx
@@ -7,27 +7,30 @@ export default function MathJax() {
 
     return (
         <>
-            <Script id='mtx' strategy="beforeInteractive">
-                {`MathJax = {
-  loader: {load: ['[tex]/physics']},
-    tex: {
-      tags: 'ams',
-      packages: {'[+]': ['physics']}
-    }
-  };
-  console.log('loaded config');`
-                }
+            <Script id="mtx" strategy="beforeInteractive" onLoad={() => console.log('loaded config')}>
+                {`
+                    MathJax = {
+                        loader: { load: ['[tex]/physics'] },
+                        tex: {
+                            tags: 'ams',
+                            packages: { '[+]': ['physics'] }
+                        }
+                    };
+                `}
             </Script>
 
-           {/*  <Script src='/js/MathJaxConfig.js' strategy="lazyOnload" onReady={() => {
+            {/*  <Script src='/js/MathJaxConfig.js' strategy="lazyOnload" onReady={() => {
                 console.log('loaded config');
             }}
                 defer /> */}
 
-            <Script src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"  strategy="afterInteractive" onReady={() => {
-                console.log('loaded MathJax');
-            }}
-                defer />
+            <Script
+                src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"
+                strategy="afterInteractive"
+                onLoad={() => console.log('loaded MathJax')}
+                defer
+            />
+
         </>
     )
 
